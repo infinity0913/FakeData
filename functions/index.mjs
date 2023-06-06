f3a5548e87e81c1308c2b50a66efa9c6e9d2de70
infinity0913 <oaoalalalppklaloa@gmail.com>
2023-06-06 14:33:19 +0500

    Init app

---
+++ functions/index.mjs
@@ -0,0 +1,19 @@
+import express from 'express';
+import serverless from 'serverless-http';
+import Cartoons from '../route/Cartoons.mjs';
+
+const app = express()
+
+
+app.get('/',(req,res) =>{
+    res.send('WELCOME')
+})
+
+app.use('/cartoons',Cartoons);
+
+const serverlessApp = serverless(app);
+
+export const handler = async (event, context) =>{
+    const  result = await serverlessApp (event,context);
+    return result;
+};
